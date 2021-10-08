c0952902346f29c0f24d71b8d41472e2544e9924
Ricardo Apú <ricardo.apu@gorillalogic.com>
2021-10-08 00:29:45 -0600

    feat: base rust api

--- rust-api/src/main.rs
+++ rust-api/src/main.rs
@@ -1,23 +1,54 @@
-use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, get, App, HttpResponse, HttpServer, Responder};
+use sqlx::postgres::PgPoolOptions;
+use serde::{Serialize,Deserialize};
+use dotenv::dotenv;
+use std::env;
+
+
+#[derive(Serialize,Deserialize)]
+struct Todo {
+    pub todo_id: i32,
+    pub user_id: Option<String>,
+    pub text: String,
+    pub done: Option<bool>,
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+#[get("/todo")]
+async fn todo(pool: web::Data<sqlx::Pool<sqlx::Postgres>>) -> impl Responder  {
+    let result =
+    sqlx::query_as!(Todo, r#"select * from todo"#)
+        .fetch_all(pool.get_ref()).await;
+    match result {
+        Ok(todos) => HttpResponse::Ok().json(todos),
+        Err(err) => {
+            println!("error fetching todos: {}", err);
+            HttpResponse::InternalServerError()
+                .body("Error trying to read all todos from database")
+        }
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    dotenv().ok();
+    let db_url = env::var("DATABASE_URL").unwrap();
+    let pool = PgPoolOptions::new()
+        .max_connections(4)
+        .connect(&db_url).await.unwrap();
+
+    let server = HttpServer::new(move || {
         App::new()
+            .data(pool.clone())
             .service(hello)
-            .service(echo)
-    })
-    .bind("127.0.0.1:8080")?
+            .service(todo)
+    });
+    println!("Rust-API listening on localhost:8080");
+    server.bind("127.0.0.1:8080")?
     .run()
     .await
 }
--- todo.md
+++ todo.md
@@ -2,12 +2,39 @@
     - [x] NodeJS + express + pg-node
     - [x] Go + Gin
     - [ ] Python + Django
-    - [ ] Rust + Actix
+    - [x] Rust + Actix
 
 - [ ] simple DB access in all langs
     - [x] NodeJS + node-pg
     - [ ] Go + go-pg
     - [ ] Python + Django
-    - [ ] Rust + diesel/rust-postgres
-  
+    - [x] Rust + sqlx
 - [x] simple TODO app-frontend
+
+#RUST
+
+$ time curl localhost:8080/todo
+[{"todo_id":9,"user_id":null,"text":"change style when done","done":false},{"todo_id":10,"user_id":null,"text":"Damn","done":false}]
+
+real    0m0.744s
+user    0m0.015s
+sys     0m0.000s
+  
+$ time curl localhost:8080/todo
+[
+    {
+        "todoId": 9,
+        "userId": null,
+        "text": "change style when done",
+        "done": false
+    },
+    {
+        "todoId": 10,
+        "userId": null,
+        "text": "Damn",
+        "done": false
+    }
+]
+real    0m0.223s
+user    0m0.000s
+sys     0m0.031s
